df91a0be08109b96f0b3bf9c62e3254574db5897
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-04-27 13:47:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,36 @@
+<?php
+$HP = 100;
+define ('MAX_DAMAGE', 50);
+define ('MIN_DAMAGE', 1);
+$damage
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cs">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    
+
+<?php
+while ($HP > 0) { ?> 
+
+<?php
+$damage = rand(MAX_DAMAGE, MIN_DAMAGE) ?>
+<p> Aktuální poškození je: <?= $damage ?> </p>
+<?php
+
+if ($HP => $damage) {
+    $HP = $HP - $damage
+} else {
+    $HP = 0
+}
+?>
+
+<p> Zbýva ti <?= $HP ?> </p>
+}
+
+<p> Konec Hry </p>
 </body>
 </html>
